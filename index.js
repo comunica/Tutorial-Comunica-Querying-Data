007402b49ad935ccc2e973b3c4d7f3d545f18cd2
Joachim Van Herwegen <joachimvh@gmail.com>
2019-01-21 09:50:56 +0100

    Allow city query parameters

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const newEngineDynamic = require('@comunica/actor-init-sparql').newEngineDynamic
 const app = express();
 const port = 3000;
 
-const query = `
+const queryTemplate = `
 SELECT ?name
 { 
     ?s dbpedia-owl:birthPlace <http://dbpedia.org/resource/Belgium>.
     ?s foaf:name ?name.
+    %CITY%
 }
 `;
 const sources = [ { type: 'hypermedia', value: 'http://fragments.dbpedia.org/2015/en' } ];
 
 app.get('/', (req, res) => {
+    const city = req.query.city;
+    // A real solution would take query injection into account!
+    let query = queryTemplate.replace('%CITY%', city ? `?s dbpedia-owl:birthPlace <${city}>.` : '');
     newEngineDynamic().then(function (myEngine) {
         myEngine.query(query, { sources: sources })
             .then((result) => {
