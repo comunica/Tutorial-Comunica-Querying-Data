dc9505f1f2771620ba2cafe332cc6c635a13eb91
Joachim Van Herwegen <joachimvh@gmail.com>
2019-01-23 09:59:36 +0100

    Update query to use multiple sources

--- index.js
+++ index.js
@@ -5,26 +5,40 @@ const app = express();
 const port = 3000;
 
 const queryTemplate = `
-SELECT ?name
+PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+PREFIX dc: <http://purl.org/dc/terms/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX schema: <http://schema.org/>
+
+SELECT ?name ?title
 { 
-    ?s dbpedia-owl:birthPlace <http://dbpedia.org/resource/Belgium>.
-    ?s foaf:name ?name.
+    ?person dbpedia-owl:birthPlace <http://dbpedia.org/resource/Belgium>.
     %CITY%
+    
+    ?viafID schema:sameAs ?person;
+            schema:name ?name.
+    ?book dc:contributor [ foaf:name ?name ];
+          dc:title ?title.
 }
 `;
-const sources = [ { type: 'hypermedia', value: 'http://fragments.dbpedia.org/2015/en' } ];
+const sources = [
+    { type: 'hypermedia', value: 'http://fragments.dbpedia.org/2016-04/en' },
+    { type: 'hypermedia', value: 'http://data.linkeddatafragments.org/viaf' },
+    { type: 'hypermedia', value: 'http://data.linkeddatafragments.org/harvard' }
+];
 
 app.get('/', (req, res) => {
     const city = req.query.city;
     // A real solution would take query injection into account!
-    let query = queryTemplate.replace('%CITY%', city ? `?s dbpedia-owl:birthPlace [ rdfs:label "${city}"@en ].` : '');
+    let query = queryTemplate.replace('%CITY%', city ? `?person dbpedia-owl:birthPlace [ rdfs:label "${city}"@en ].` : '');
     newEngineDynamic().then(function (myEngine) {
-        myEngine.query(query, { sources: sources })
+        myEngine.query(query, { sources: sources.slice() })
             .then((result) => {
 
                 res.set('content-type', 'text/plain; charset=utf-8');
 
-                result.bindingsStream.on('data', data => res.write(data.get('?name').value + '\n') );
+                result.bindingsStream.on('data', data => res.write(`${data.get('?name').value}: ${data.get('?title').value}\n`) );
                 result.bindingsStream.on('end', () => res.end());
 
             });
