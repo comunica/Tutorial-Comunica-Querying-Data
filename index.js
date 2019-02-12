05e0ea095efb66cc45fc9a6dd503844c2cfcdd97
Joachim Van Herwegen <joachimvh@gmail.com>
2019-02-12 15:23:00 +0100

    Use dynamic city parameter

--- index.js
+++ index.js
@@ -5,21 +5,33 @@ const app = express();
 const port = 3000;
 const engine = newEngine();
 
-const query = `
+const queryTemplate = `
 PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
 SELECT ?name
 { 
     ?person dbpedia-owl:birthPlace <http://dbpedia.org/resource/Belgium>;
             foaf:name ?name.
+    
+    %CITY%
 }
 `;
 const sources = [
     { type: 'hypermedia', value: 'https://fragments.dbpedia.org/2016-04/en' }
 ];
 
+function cleanInput(str) {
+    if (str)
+        return str.replace(/["\\]/g, '');
+    else
+        return '';
+}
+
 app.get('/', (req, res) => {
+    const city = cleanInput(req.query.city);
+    let query = queryTemplate.replace('%CITY%', city ? `?person dbpedia-owl:birthPlace [ rdfs:label "${city}"@en ].` : '');
     engine.query(query, { sources: sources }).then(result => {
         res.set('content-type', 'text/plain; charset=utf-8');
 
