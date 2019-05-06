8825b6c7d00897344ff2103b6a6c797b74556a89
Joachim Van Herwegen <joachimvh@gmail.com>
2019-05-06 11:27:11 +0200

    Reset to initial state

--- index.js
+++ index.js
@@ -18,26 +18,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 async function handleData () {
-    const query = `
-    PREFIX dbpedia: <http://dbpedia.org/resource/>
-    PREFIX dbbprop: <http://dbpedia.org/property/>
-    
-    SELECT * WHERE {
-        ?s dbpprop:occupation dbpedia:Computer_scientist
-    }
-    `;
-    const sources = [
-        { type: "file", value: "http://localhost:8080/data.jsonld" },
-        { type: "hypermedia", value: "http://fragments.dbpedia.org/2016-04/en" },
-    ];
-    const result = await engine.query(query, { sources });
-    const results = [];
-    result.bindingsStream.on('data', data => {
-        results.push(data.get('?s').value);
-    });
-    return new Promise(resolve => {
-        result.bindingsStream.on('end', () => {
-            resolve(results);
-        })
-    });
+    /*
+        ADD CODE HERE
+     */
 }
